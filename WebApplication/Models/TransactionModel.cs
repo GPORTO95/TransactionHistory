e4a47a3966d48d10ac876b1fde9bbfafa1ff05c5
Gabriel Porto <gr_porto@hotmail.com>
2020-04-06 19:36:01 -0300

    --Add partial view --Add ajax submit --Add CRUD

--- WebApplication/Models/TransactionModel.cs
+++ WebApplication/Models/TransactionModel.cs
@@ -12,18 +12,30 @@ namespace WebApplication.Models
         [Key]
         public int TransactionId { get; set; }
 
+        [MaxLength(12)]
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Account Number")]
         [Column(TypeName = "nvarchar(12)")]
         public string AccountNumber { get; set; }
 
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Beneficary Name")]
         [Column(TypeName = "nvarchar(100)")]
         public string BeneficaryName { get; set; }
 
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Bank Name")]
         [Column(TypeName = "nvarchar(100)")]
         public string BankName { get; set; }
-
+        
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "SWIFT Cod")]
         [Column(TypeName = "nvarchar(11)")]
         public string SWIFTCod { get; set; }
+        
+        [Required(ErrorMessage = "This field is required")]
         public int Amount { get; set; }
+        
         public DateTime Date { get; set; }
     }
 }
