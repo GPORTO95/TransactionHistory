2db239f95261d5afde1f6f182de4e57cbaf95979
Gabriel Porto <gr_porto@hotmail.com>
2020-04-06 18:26:26 -0300

    -DB Add migration -Class Transaction -Controller Transaction

---
+++ WebApplication/Models/TransactionModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class TransactionModel
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Column(TypeName = "nvarchar(12)")]
+        public string AccountNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string BeneficaryName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string BankName { get; set; }
+
+        [Column(TypeName = "nvarchar(11)")]
+        public string SWIFTCod { get; set; }
+        public int Amount { get; set; }
+    }
+}
